c7b0e66a4fad77b07d2efdda89e50b96f04e4243
NguyenDinhNhan010902 <115056699+NguyenDinhNhan010902@users.noreply.github.com>
2024-10-09 11:48:33 +0700

    môi trường code

---
+++ BE/src/main/java/com/qlda/timdobat/TimdobatApplication.java
@@ -0,0 +1,13 @@
+package com.qlda.timdobat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimdobatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimdobatApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/qlda/timdobat/TimdobatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlda.timdobat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimdobatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
